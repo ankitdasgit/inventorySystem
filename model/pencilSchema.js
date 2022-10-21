73dbbd720dfefe6a1231ae3a2cbbafd5d5a61774
ankit das <ankit.das2019@sait.ac.in>
2022-10-21 11:17:39 +0530

    get request for pencilSchema

---
+++ model/pencilSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const PencilSchema = new mongoose.Schema({
+  itemName: String,
+  price: Number,
+  quantity: Number,
+});
+
+module.exports = new mongoose.model("pencil",PencilSchema);
